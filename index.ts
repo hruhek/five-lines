11d0772f9a54789639eccf60824c0bddcad23f82
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-12-30 17:41:14 +0100

    Added FallStrategy class

--- index.ts
+++ index.ts
@@ -34,6 +34,21 @@ class Resting implements FallingState {
   }
 }
 
+class FallStrategy {
+  constructor(private falling: FallingState) {}
+  getFalling() { return this.falling; }
+  update(tile: Tile, x: number, y: number) {
+    this.falling = map[y + 1][x].isAir()? new Falling(): new Resting();
+    this.drop(tile, x, y);
+  }
+  private drop(tile: Tile, x: number, y: number) {
+    if (this.falling.isFalling()) {
+      map[y + 1][x] = tile;
+      map[y][x] = new Air();
+    }
+  }
+}
+
 interface Tile {
   isAir(): boolean;
   isLock1(): boolean;
@@ -59,7 +74,10 @@ class Air implements Tile {
 }
 
 class Box implements Tile {
-  constructor(private falling: FallingState) {}
+  private fallStrategy: FallStrategy;
+  constructor(falling: FallingState) {
+    this.fallStrategy = new FallStrategy(falling);
+  }
   isAir(): boolean {return false;}
   isLock1(): boolean {return false;}
   isLock2(): boolean {return false;}
@@ -68,17 +86,11 @@ class Box implements Tile {
     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
   moveHorizontal(dx: number) {
-    this.falling.moveHorizontal(this, dx)
+    this.fallStrategy.getFalling().moveHorizontal(this, dx)
   }
   moveVertical(dy: number) {}
   update(x: number, y: number) {
-    if (map[y + 1][x].isAir()) {
-      this.falling = new Falling();
-      map[y + 1][x] = this;
-      map[y][x] = new Air();
-    } else if (this.falling.isFalling()) {
-      this.falling = new Resting();
-    }
+    this.fallStrategy.update(this, x, y);
   }
 }
 
@@ -174,7 +186,10 @@ class Player implements Tile {
 }
 
 class Stone implements Tile {
-  constructor(private falling: FallingState) {}
+  private fallStrategy: FallStrategy;
+  constructor(falling: FallingState) {
+    this.fallStrategy = new FallStrategy(falling);
+  }
   isAir(): boolean {return false}
   isLock1(): boolean {return false;}
   isLock2(): boolean {return false;}
@@ -183,17 +198,11 @@ class Stone implements Tile {
     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
   moveHorizontal(dx: number) {
-    this.falling.moveHorizontal(this, dx)
+    this.fallStrategy.getFalling().moveHorizontal(this, dx)
   }
   moveVertical(dy: number) {}
   update(x: number, y: number) {
-    if (map[y + 1][x].isAir()) {
-      this.falling = new Falling();
-      map[y + 1][x] = this;
-      map[y][x] = new Air();
-    } else if (this.falling.isFalling()) {
-      this.falling = new Resting();
-    }
+    this.fallStrategy.update(this, x, y);
   }
 }
 
