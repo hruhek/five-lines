4d6c4beeb776752876713face8c2778e6e983d30
Hrvoje Ruhek <trulija@gmail.com>
2022-01-01 13:27:43 +0100

    Finished of counter example.

--- counter.ts
+++ counter.ts
@@ -1,12 +1,16 @@
-let counter = 0;
-
-function incrementCounter() {
-  counter++;
+function main() {
+  let counter = new Counter();
+  for (let i = 0; i < 20; i++) {
+    counter.incrementCounter();
+    console.log(counter.getCounter());
+  }
 }
 
-function main() {
-  for (let i = 0; i < 20; i++) {
-    incrementCounter();
-    console.log(counter);
+class Counter {
+  private counter = 0;
+  getCounter() { return this.counter; }
+  setCounter(c: number) { this.counter = c; }
+  incrementCounter() {
+    this.counter = this.counter + 1;
   }
 }
